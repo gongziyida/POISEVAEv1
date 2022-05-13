fix kl tentative

poisevae/POISE_VAE_gibbs_autograd.py
@@ -208,13 +208,15 @@ class POISEVAE(nn.Module):
                 ret = self.encoders[i](xi)
                 param1.append(ret[0].view(batch_size, -1))
                 sign = 1 if self.enc_config == 'mu/var' else -1
-                param2.append(sign * torch.exp(ret[1].view(batch_size, -1)))
+                # param2.append(sign * torch.exp(ret[1].view(batch_size, -1)))
+                param2.append(sign * nn.functional.softplus(ret[1].view(batch_size, -1)))
                 if len(ret) == 2:
                     param1_.append(None)
                     param2_.append(None)
                 elif len(ret) == 4:
                     param1_.append(ret[2].view(batch_size, -1))
-                    param2_.append(sign * torch.exp(ret[3].view(batch_size, -1)))
+                    # param2_.append(sign * torch.exp(ret[3].view(batch_size, -1)))
+                    param2_.append(sign * nn.functional.softplus(ret[3].view(batch_size, -1)))
         return param1, param2, param1_, param2_
     
     def decode(self, z):
@@ -294,7 +296,7 @@ class POISEVAE(nn.Module):
         t2 = [-torch.exp(t2_hat) for t2_hat in self.t2_hat]
         return self.t1, t2
     
-    def forward(self, x, n_gibbs_iter=15, kl_weight=1):
+    def forward(self, x, n_gibbs_iter=15, kl_weight=1, detach_G=False):
         """
         Return
         ------
@@ -318,17 +320,17 @@ class POISEVAE(nn.Module):
             param1, param2, param1_, param2_ = self.encode(self.mask_missing(x))
         else:
             param1, param2, param1_, param2_ = self.encode(x)
-        # if param1[0] is not None and param1[1] is not None:
-        #     print('mu max:', torch.abs(param1[0]).max().item(), 'mu mean:', torch.abs(param1[0]).mean().item())
-        #     print('mup max:', torch.abs(param1[1]).max().item(), 'mup mean:', torch.abs(param1[1]).mean().item())
-        #     print('var min:', torch.abs(param2[0]).min().item(), 'var mean:', torch.abs(param2[0]).mean().item())
-        #     print('varp min:', torch.abs(param2[1]).min().item(), 'varp mean:', torch.abs(param2[1]).mean().item())
-        #     assert torch.isnan(param1[0]).sum() == 0
+        if param1[0] is not None and param1[1] is not None:
+            print('nu1 max:', torch.abs(param1[0]).max().item(), 'nu1 mean:', torch.abs(param1[0]).mean().item())
+            print('nu1p max:', torch.abs(param1[1]).max().item(), 'nu1p mean:', torch.abs(param1[1]).mean().item())
+            print('nu2 min:', torch.abs(param2[0]).min().item(), 'nu2 mean:', torch.abs(param2[0]).mean().item())
+            print('nu2p min:', torch.abs(param2[1]).min().item(), 'nu2p mean:', torch.abs(param2[1]).mean().item())
+            assert torch.isnan(param1[0]).sum() == 0
         
-        G = self.get_G()
+        G = self.get_G().detach() if detach_G else self.get_G()
         _, t2 = self.get_t()
     
-        z_posteriors, kl = self._sampling(G.detach(), param1, param2, param1_, param2_, t2, 
+        z_posteriors, kl = self._sampling(G, param1, param2, param1_, param2_, t2, 
                                           n_iterations=n_gibbs_iter)
         
         assert torch.isnan(G).sum() == 0
@@ -375,11 +377,11 @@ class POISEVAE(nn.Module):
             'z': z_posteriors, 'x_rec': x_rec, 'param1': param1, 'param2': param2,
             'total_loss': total_loss, 'rec_losses': recs, 'KL_loss': kl
         }
-        # if param1[0] is not None and param1[1] is not None:
-        #     print('total loss:', total_loss.item(), 'kl term:', kl.item())
-        #     print('rec1 loss:', recs[0].item() / batch_size / n_gibbs_iter, 
-        #           'rec2 loss:', recs[1].item() / batch_size / n_gibbs_iter)
-        #     print()
+        if param1[0] is not None and param1[1] is not None:
+            print('total loss:', total_loss.item(), 'kl term:', kl.item())
+            print('rec1 loss:', recs[0].item() / batch_size / n_gibbs_iter, 
+                  'rec2 loss:', recs[1].item() / batch_size / n_gibbs_iter)
+            print()
         return results
 
     
@@ -390,7 +392,7 @@ class POISEVAE(nn.Module):
         
         nones = [None] * len(self.latent_dims)
         
-        z_posteriors, kl = self._sampling(G.detach(), nones, nones, t2, n_iterations=n_gibbs_iter)
+        z_posteriors, kl = self._sampling(G, nones, nones, t2, n_iterations=n_gibbs_iter)
         x_rec = self.decode(z_posteriors)
         
         for i in range(self.M):

poisevae/kl_divergence_calculator.py
@@ -100,54 +100,64 @@ class KLDDerivative:
         nu1_, nu2_ = init_posterior(nu1_, nu2_, mu_, var_, self.enc_config)
         nu1, nu2 = new_nu(nu1, nu1_), new_nu(nu2, nu2_)
         ## Creating Sufficient statistics
-        T_priors, T_posts, nus = [], [], []
-        for i in range(len(z)):
-            T_priors.append(torch.cat((z_priors[i], torch.square(z_priors[i])), -1))
-            T_posts.append(torch.cat((z[i], torch.square(z[i])), -1))
-            if nu1[i] is None: 
-                if nu2[i] is not None:
-                    raise RuntimeError('Unmatched nus')
-                
-                nus.append(torch.zeros(T_posts[-1].shape[0], T_posts[-1].shape[2]).to(self.device))
-            else:
-                nus.append(torch.cat((nu1[i], nu2[i]), -1))
+        T_prior = torch.cat((z_priors[0], torch.square(z_priors[0])), -1)
+        Tp_prior = torch.cat((z_priors[1], torch.square(z_priors[1])), -1)
+        T_post = torch.cat((z[0], torch.square(z[0])), -1)
+        Tp_post = torch.cat((z[1], torch.square(z[1])), -1)
+
+        nu = torch.cat((nu1[0], nu2[0]), -1) if nu1[0] is not None else \
+             torch.zeros(T_post.shape[0], T_post.shape[2]).to(self.device)
+        nup = torch.cat((nu1[1], nu2[1]), -1) if nu1[1] is not None else \
+              torch.zeros(Tp_post.shape[0], Tp_post.shape[2]).to(self.device)
         # print('zpost max:', torch.abs(z[0]).max().item(), 'zpost mean:', torch.abs(z[0]).mean().item())
         # print('zpostp max:', torch.abs(z[1]).max().item(), 'zpostp mean:', torch.abs(z[1]).mean().item())
         # print('zprior max:', torch.abs(z_priors[0]).max().item(), 'zprior mean:', torch.abs(z_priors[0]).mean().item())
         # print('zpriorp max:', torch.abs(z_priors[1]).max().item(), 'zpriorp mean:', torch.abs(z_priors[1]).mean().item())
-        # TODO: make it generic for > 2 latent spaces
-        batch_size = z[0].shape[0]
-        if z[1].shape[0] != batch_size:
-            raise ValueError('Batch size must match.')
         
-        T1_prior_unsq = T_priors[0].unsqueeze(-1)
-        T2_prior_unsq = T_priors[1].unsqueeze(-2)
-        T1_post_unsq  = T_posts[0].unsqueeze(-1) 
-        T2_post_unsq  = T_posts[1].unsqueeze(-2)
+        with torch.no_grad():
+            T_post_diff = T_post - T_post.mean(1, keepdim=True)
+            Tp_post_diff = Tp_post - Tp_post.mean(1, keepdim=True)
+            n = T_post.shape[1]
+            assert Tp_post.shape[1] == n
+            n = n - 1 if n > 1 else 1
+            cov = torch.bmm(T_post_diff.transpose(1, 2), T_post_diff) / n
+            covp = torch.bmm(Tp_post_diff.transpose(1, 2), Tp_post_diff) / n
+            dnu = torch.bmm(nu.unsqueeze(1), cov).squeeze(1)
+            dnup = torch.bmm(nup.unsqueeze(1), covp).squeeze(1)
+            
+            T_prior_outer = (T_prior.unsqueeze(-1) * Tp_prior.unsqueeze(-2))
+            T_post_outer = (T_post.unsqueeze(-1) * Tp_post.unsqueeze(-2))
+            # ET_prior_outer, ET_post_outer = T_prior_outer.mean(1), T_post_outer.mean(1)
+            nuT = (nu.unsqueeze(1) * T_post).sum(-1, keepdim=True).unsqueeze(-1)
+            nupTp = (nup.unsqueeze(1) * Tp_post).sum(-1, keepdim=True).unsqueeze(-1) # (batch, n_gibbs, 1,1)
+            dG = (T_post_outer * (nuT + nupTp)).mean(1) \
+               - T_post_outer.mean(1) * (nuT + nupTp).mean(1) \
+               + T_prior_outer.mean(1) - T_post_outer.mean(1)
+            
+        part0 = (dnu * nu).sum(dim=-1)
+        part1 = (dnup * nup).sum(dim=-1)
+        part2 = (dG * G).sum(dim=(-1, -2))
         
-        T_prior_outer = (T1_prior_unsq * T2_prior_unsq).mean(1)
-        T_post_outer = (T1_post_unsq * T2_post_unsq).mean(1)
+        # part2 = (T_prior_outer.detach() * G).sum(dim=(-1, -2)) - \
+        #         (T_post_outer.detach() * G).sum(dim=(-1, -2))
         
-        T_post_mean = [T_posts[0].mean(1), T_posts[1].mean(1)]
+        # T_post_mean, Tp_post_mean = T_post.mean(1), Tp_post.mean(1)
         
-        part0 = (nus[0] * T_post_mean[0]).sum(dim=-1) + \
-                (nus[1] * T_post_mean[1]).sum(dim=-1)
+#         part0 = (nu * T_post_mean).sum(dim=-1) + \
+#                 (nup * Tp_post_mean).sum(dim=-1)
         
-        part1 = (nus[0] * T_post_mean[0].detach()).sum(dim=-1) + \
-                (nus[1] * T_post_mean[1].detach()).sum(dim=-1)
+#         part1 = (nu * T_post_mean.detach()).sum(dim=-1) + \
+#                 (nup * Tp_post_mean.detach()).sum(dim=-1)
         
-        part2 = (T_prior_outer.detach() * G).sum(dim=(-1, -2)) - \
-                (T_post_outer.detach() * G).sum(dim=(-1, -2))
+#         part2 = ((T_prior_outer - T_post_outer) * G.unsqueeze(0)).sum(dim=(-1, -2))
         
+        print('p0 mean', torch.abs(part0.detach()).mean().item(), 'p1 mean', torch.abs(part1.detach()).mean().item())
+        print('p2 mean', torch.abs(part2.detach()).mean().item(), 'dg mean', torch.abs((T_prior_outer - T_post_outer).detach()).mean().item())
+        print('tpo mean', torch.abs(T_prior_outer.detach()).mean().item(), 'tqo mean', torch.abs(T_post_outer.detach()).mean().item())
         assert len(part0.shape) == 1 and len(part1.shape) == 1 and len(part2.shape) == 1
         if self.reduction == 'mean':
-            return part0.mean() - part1.mean() + part2.mean()
+            return part0.mean() + part1.mean() + part2.mean()
         elif self.reduction == 'sum':
-            return part0.sum() - part1.sum() + part2.sum()
+            return part0.sum() + part1.sum() + part2.sum()
         else:
             raise NotImplementedError
-    
-    def dot_product(self, tensor_1, tensor_2):
-        out = torch.sum(torch.mul(tensor_1, tensor_2))
-        return out
-    

poisevae/utils/functions.py
@@ -20,7 +20,7 @@ def _log(results, mode, writer, epoch):
     
     
 def train(model, joint_dataloader, optimizer, epoch, kl_weight, n_gibbs_iter, writer=None,
-          mask_missing=None, device=_device, dtype=torch.float32):
+          mask_missing=None, detach_G=False, device=_device, dtype=torch.float32):
     '''
     Parameters
     ----------
@@ -52,13 +52,13 @@ def train(model, joint_dataloader, optimizer, epoch, kl_weight, n_gibbs_iter, wr
         if mask_missing is not None:
             results = model(mask_missing(
                             [data[i].to(device=device, dtype=dtype) for i in range(model.M)]), 
-                            kl_weight=kl_weight, n_gibbs_iter=n_gibbs_iter)
+                            kl_weight=kl_weight, n_gibbs_iter=n_gibbs_iter, detach_G=detach_G)
         else:
             results = model([data[i].to(device=device, dtype=dtype) for i in range(model.M)], 
-                            kl_weight=kl_weight, n_gibbs_iter=n_gibbs_iter)
+                            kl_weight=kl_weight, n_gibbs_iter=n_gibbs_iter, detach_G=detach_G)
             
         results['total_loss'].backward() 
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 50)
+        # torch.nn.utils.clip_grad_norm_(model.parameters(), 100)
         optimizer.step()
         _log(results, 'train', writer, epoch * len(joint_dataloader) + k)
         